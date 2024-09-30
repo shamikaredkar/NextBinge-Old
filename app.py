a5e81f0c8c9c1211fb30297c2b43d91dd08b6654
Shamika Redkar <shamikaredkar91@gmail.com>
2024-09-30 11:05:56 -0400

    Imported similarity matrix from collab notebook

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:11]
+    recommended_movies = []
     for i in movies_list:
-        print(new_df.iloc[i[0]].title)
+        recommended_movies.append(movies.iloc[i[0].title])
+    return recommended_movies
 
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
 st.title("NextBinge") 
 option = st.selectbox('Select a movie',movies['title'].values)
 
