650b71d5058b98efaf448d77d48c856d735a642b
Shamika Redkar <shamikaredkar91@gmail.com>
2024-09-30 12:52:03 -0400

    Added hover effect

--- app.py
+++ app.py
@@ -9,20 +9,31 @@ def fetch_poster(movie_id):
     data = data.json()
     poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    # Get tagline
+    tagline = data.get('tagline', 'No tagline available.')
+    
+    # Get genres
+    genres = [genre['name'] for genre in data.get('genres', [])]
+    genres_str = ", ".join(genres) if genres else 'No genres available.'
+    return full_path, tagline, genres_str
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
+    recommended_movie_taglines = []
+    recommended_movie_genres = []
+    
     for i in distances[1:11]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
+        poster, tagline, genres = fetch_poster(movie_id)
+        recommended_movie_posters.append(poster)
         recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_taglines.append(tagline)
+        recommended_movie_genres.append(genres)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters, recommended_movie_taglines, recommended_movie_genres
 
 
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
@@ -32,44 +43,64 @@ similarity = pickle.load(open('similarity.pkl', 'rb'))
 movie_list = movies['title'].values
 
 
-st.title("NextBinge") 
+# Add custom CSS for hover effect
+st.markdown("""
+    <style>
+    .movie-container {
+        position: relative;
+        width: 100%;
+    }
+    .movie-title {
+        position: absolute;
+        bottom: 10px;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        color: white;
+        background-color: rgba(0, 0, 0, 0.7);
+        padding: 5px;
+        opacity: 0;
+        transition: opacity 0.3s ease;
+    }
+    .movie-container:hover .movie-title {
+        opacity: 1;
+    }
+    .movie-poster {
+        width: 100%;
+        height: auto;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+st.title("NextBinge")
 option = st.selectbox("Type or select a movie from the dropdown", movie_list)
 
 if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(option)
+    recommended_movie_names, recommended_movie_posters, recommended_movie_taglines, recommended_movie_genres = recommend(option)
     
     # First row of 5 columns
     col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-    
+    for i, col in enumerate([col1, col2, col3, col4, col5]):
+        with col:
+            st.markdown(f"""
+            <div class="movie-container">
+                <img class="movie-poster" src="{recommended_movie_posters[i]}" alt="{recommended_movie_names[i]}">
+                <div class="movie-title">{recommended_movie_names[i]}</div>
+            </div>
+            """, unsafe_allow_html=True)
+            st.caption(recommended_movie_taglines[i])
+            st.caption(f"Genres: {recommended_movie_genres[i]}")
+
     # Second row of 5 columns
     col6, col7, col8, col9, col10 = st.columns(5)
-    with col6:
-        st.text(recommended_movie_names[5])
-        st.image(recommended_movie_posters[5])
-    with col7:
-        st.text(recommended_movie_names[6])
-        st.image(recommended_movie_posters[6])
-    with col8:
-        st.text(recommended_movie_names[7])
-        st.image(recommended_movie_posters[7])
-    with col9:
-        st.text(recommended_movie_names[8])
-        st.image(recommended_movie_posters[8])
-    with col10:
-        st.text(recommended_movie_names[9])
-        st.image(recommended_movie_posters[9])
+    for i, col in enumerate([col6, col7, col8, col9, col10], start=5):
+        with col:
+            st.markdown(f"""
+            <div class="movie-container">
+                <img class="movie-poster" src="{recommended_movie_posters[i]}" alt="{recommended_movie_names[i]}">
+                <div class="movie-title">{recommended_movie_names[i]}</div>
+            </div>
+            """, unsafe_allow_html=True)
+            st.caption(recommended_movie_taglines[i])
+            st.caption(f"Genres: {recommended_movie_genres[i]}")
